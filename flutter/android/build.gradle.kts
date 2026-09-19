9dde7e99ffc14c18e9d9c2b6442a2a6714762c52
Bot Hunting <fariszubaidi1@gmail.com>
2026-09-19 14:19:22 +0700

    zubed

--- flutter/android/build.gradle.kts
+++ flutter/android/build.gradle.kts
@@ -1,11 +1,11 @@
 buildscript {
-    ext.kotlin_version = '1.9.20' // Jangan gunakan 2.2.20
+    ext.kotlin_version = '1.9.20'
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.1.0'
+        classpath 'com.android.tools.build:gradle:7.4.2' // <-- Ubah di sini
         classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
         classpath 'com.google.gms:google-services:4.3.15'
     }
@@ -17,27 +17,41 @@ allprojects {
         mavenCentral()
     }
 }
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
+
+rootProject.buildDir = '../build'
+subprojects {
+    project.buildDir = "${rootProject.buildDir}/${project.name}"
+}
+subprojects {
+    project.evaluationDependsOn(':app')
+}
+
+tasks.register("clean", Delete) {
+    delete rootProject.buildDir
+}
+android {
+    namespace = "com.example.kirana_tanjung"
+    compileSdk = 34 // Optimasi untuk Android 14
+
+    // HAPUS baris ndkVersion agar menggunakan default Flutter SDK
+    // ndkVersion "25.0.3" 
+
+    defaultConfig {
+        applicationId = "com.example.kirana_tanjung"
+        // Sinkronisasi dengan requirements package_info_plus & printing
+        minSdk = 21 
+        targetSdk = 34
+        versionCode = flutterVersionCode.toInteger()
+        versionName = flutterVersionName
     }
-}
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.debug
+            // Optimasi ukuran APK
+            minifyEnabled true
+            shrinkResources true
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
+    }
 }
